9244d8433fce0d54ce6e50687d9a8624a281b987
Maks <Zhdanov-1997@inbox.ru>
2022-11-20 19:52:48 +0300

    incomplete addition emplooyee

--- src/components/add-form/add-form.jsx
+++ src/components/add-form/add-form.jsx
@@ -1,21 +1,33 @@
+import { useState } from 'react';
 import './add-form.scss'
 
-const AddForm = () => {
-  
+const AddForm = (addUsers, employeeData) => {
+    const [inputValueUser, setInputValueUser] = useState('');
+    const [inputValueSalary, setInputValueSalary] = useState('');
+ 
+    const onSubmitValue = (e) => {
+        e.preventDefault();
+    }
+
     return (
         <div className="add-form">
             <h3>Добавьте нового сотрудника</h3>
-            <form className="app-add-form d-flex">
+            <form onSubmit={onSubmitValue} className="app-add-form d-flex" >
                 <input type="text"
                        className="form-control"
-                       placeholder="Как его зовут?"/>
+                       placeholder="Как его зовут?"
+                       value={inputValueUser}
+                       onChange={(e) => setInputValueUser(e.target.value)}/>
 
                 <input type="number"
                        className="form-control"
-                       placeholder="З/П в $?"/>
+                       placeholder="З/П в $?"
+                       value={inputValueSalary}
+                       onChange={(e) => setInputValueSalary(e.target.value) }/>
 
-                <button type="submit"
-                        className="btn btn-outline-light">Добавить</button>
+                <button 
+                        className="btn btn-outline-light"
+                        type='submit'>Добавить</button>
             </form>
         </div>
     )
--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,27 +1,40 @@
 import { useState } from "react"
-
+import { v4 as uuidv4 } from 'uuid';
 import AppInfo from "../app-info/app-info"
 import SearchPanel from "../search-panel/search-panel"
 import AppFilter from "../app-filter/app-filter"
 import AllEmployees from "../all-employees/all-employees"
 import AddForm from "../add-form/add-form"
-
 import './app.scss'
-
+  
 
 const App = () => {
-    const data = [
+    const dataEmployee = [
         {id: 1, name: "Maks M.", salary: 1000, increase: true, like: true},
         {id: 2, name: "Dima R.", salary: 2000, increase: false, like: false},
-        {id: 3, name: "Olga T.", salary: 4000, increase: true, like: false}
+        {id: 3, name: "Olga T.", salary: 4000, increase: true, like: false},
     ];
 
 
-    const [users, setUsers] = useState(data);
+    const [users, setUsers] = useState(dataEmployee);
     const removeItem = id => {
         setUsers(prevState => prevState.filter(el => el.id !== id))
-     }
+    }
 
+    
+    const [employeeData, setEmployeData] = useState()
+    const addUser = () => {
+        const newUsers = {
+            id: uuidv4(),
+            name: '',
+            salary: '',
+            increase: true,
+            like: false
+        };
+        setEmployeData(prevState => [...prevState, newUsers])
+    }
+
+  
     return(
         <div className="app">
             <AppInfo/>
@@ -30,7 +43,7 @@ const App = () => {
                     <AppFilter/>
                 </div>
             <AllEmployees removeItem={removeItem} users={users}/>
-            <AddForm />
+            <AddForm addUser={addUser} employeeData={employeeData}/>
         </div>
     )
 }
